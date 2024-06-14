f73a9a19d1f894b3dc4a98a3b5728a4cb5d9e89d
Christopher Mangion <christopher.mangion@hotmail.com>
2024-06-14 14:34:17 +1000

    Added refseq as a source Refseq source now downloads refseq summary and enriches from api Added ProgressBar as an independent object Updated Downloader to use new ProgressBar instead of handling itself Refseq processing uses ProgressBar

---
+++ src/lib/tools/progressBar.py
@@ -0,0 +1,15 @@
+class ProgressBar:
+    def __init__(self, barLength: int, processName: str = "Progress"):
+        self.barLength = barLength
+        self.processName = processName
+        self._loading = "-\\|/"
+        self._pos = 0
+
+    def render(self, completion: float) -> None:
+        length = int(completion * self.barLength)
+        print(f"> {self.processName} ({self._loading[self._pos]}): [{length * '='}{(self.barLength - length) * '-'}]", end="\r")
+        self._pos = (self._pos + 1) % 4
+
+        if length == self.barLength: # Completed:
+            print("\n")
+    
