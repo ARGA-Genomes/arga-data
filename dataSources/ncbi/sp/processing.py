db0769141879322fe72f69367c6c77f1b107da93
Christopher Mangion <christopher.mangion@hotmail.com>
2025-04-15 10:56:16 +1000

    Updated ncbi-sp to use entrez

--- dataSources/ncbi/sp/processing.py
+++ dataSources/ncbi/sp/processing.py
@@ -2,30 +2,39 @@ from pathlib import Path
 import requests
 import lib.xml as xml
 from bs4 import BeautifulSoup
-from io import BytesIO
 from lib.bigFileWriter import BigFileWriter
 import pandas as pd
 from lib.progressBar import SteppableProgressBar
-import concurrent.futures as cf
-
-def _worker(ids: list[str]) -> list[dict]:
-    xmlData = requests.get(f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id={','.join(ids)}&rettype=fasta&retmode=xml")
-    return [xml.flattenElement(element) for element in xml.xmlGenerator(BytesIO(xmlData.content))]
+import lib.downloading as dl
 
 def retrieve(outputFilePath: Path):
-    response = requests.get(f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=nuccore&term=\"+sp.\"+AND+country%3DAustralia+NOT+viruses+NOT+bacteria+NOT+archaea&retmax=500000")
+    baseURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+    query = "\"+sp.\"+AND+country%3DAustralia+NOT+viruses+NOT+bacteria+NOT+archaea"
+
+    response = requests.get(f"{baseURL}esearch.fcgi?db=nuccore&term={query}&usehistory=y")
     soup = BeautifulSoup(response.content, "xml")
-    ids = [id.text for id in soup.find_all("Id")]
-    
-    recordsPerCall = 200
-    totalCalls = (len(ids) / recordsPerCall).__ceil__()
-    progress = SteppableProgressBar(totalCalls)
 
-    records = []
-    with cf.ThreadPoolExecutor() as executor:
-        futures = (executor.submit(_worker, ids[call*recordsPerCall:(call+1)*recordsPerCall]) for call in range(totalCalls))
-        for future in cf.as_completed(futures):
-            records.extend(future.result())
-            progress.update()
+    count = soup.find("Count").text
+    query = soup.find("QueryKey").text
+    webenv = soup.find("WebEnv").text
 
-    pd.DataFrame.from_dict(records).to_csv(outputFilePath.parent / "nuccore.csv", index=False)
+    chunkSize = 10000
+    totalCalls = (int(count) / chunkSize).__ceil__()
+
+    writer = BigFileWriter(outputFilePath.parent / "nuccore.csv")
+    writer.subfileDir.mkdir(exist_ok=True)
+    tempFile = writer.subfileDir / "download.xml"
+
+    for call in range(totalCalls):
+        dl.download(f"{baseURL}efetch.fcgi?db=nuccore&WebEnv={webenv}&query_key={query}&retstart={call*chunkSize}&retmax={chunkSize}&rettype=fasta&retmode=xml", tempFile, chunkSize=256*1024*1024)
+
+        records = []
+        for element in xml.xmlGenerator(tempFile):
+            records.append(xml.flattenElement(element))
+
+        writer.writeDF(pd.DataFrame.from_records(records))
+        tempFile.unlink()
+
+        print(f"Completed section {call+1} / {totalCalls}")
+
+    writer.oneFile()
