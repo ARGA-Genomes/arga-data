d82d97a8854e6239d838b47ae6c757f30c950304
Christopher Mangion <christopher.mangion@hotmail.com>
2025-05-27 12:29:48 +1000

    Added gbif as a data source

---
+++ dataSources/gbif/api/processing.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+import lib.downloading as dl
+from lib.secrets import secrets
+import requests
+
+def collect(outputFilePath: Path) -> None:
+    baseURL = "https://api.gbif.org/v1"
+    requestEndpoint = "/occurrence/download/request"
+    statusEndpoint = "/occurrence/download"
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    formData = {
+        "creator": secrets.gbif.creator,
+        "notificationAddresses": [
+            secrets.general.email
+        ],
+        "sendNotification": "false",
+        "format": "SIMPLE_CSV",
+        "predicate": {
+            "type": "and",
+            "predicates": [
+                {
+                    "type": "in",
+                    "key": "BASIS_OF_RECORD",
+                    "values": [
+                        "MATERIAL_SAMPLE",
+                        "MATERIAL_CITATION",
+                        "PRESERVED_SPECIMEN",
+                        "FOSSIL_SPECIMEN",
+                        "LIVING_SPECIMEN",
+                        "OCCURRENCE"
+                    ]
+                },
+                {
+                    "type": "equals",
+                    "key": "COUNTRY",
+                    "value": "AU"
+                },
+                {
+                    "type": "equals",
+                    "key": "HAS_COORDINATE",
+                    "value": "true"
+                },
+                {
+                    "type": "equals",
+                    "key": "HAS_GEOSPATIAL_ISSUE",
+                    "value": "false"
+                },
+                {
+                    "type": "equals",
+                    "key": "OCCURRENCE_STATUS",
+                    "value": "PRESENT"
+                },
+            ]
+        }
+    }
+
+    strFormData = str(formData).replace(" ", "").replace("'", '"')
+    response = requests.post(f"{baseURL}{requestEndpoint}", headers=headers, data=strFormData, auth=dl.buildAuth(secrets.general.email, secrets.gbif.password))
+    requestID = response.text
+
+    statusURL = f"{baseURL}{statusEndpoint}/{requestID}"
+    dl.asyncRunner(statusURL, "status", "SUCCEEDED", "downloadLink", outputFilePath)
