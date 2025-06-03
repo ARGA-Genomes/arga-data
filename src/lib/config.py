ab86c8b477a195dc09e4d473f09475f2864ca251
Christopher Mangion <christopher.mangion@hotmail.com>
2025-06-03 10:01:41 +1000

    Added a fix for TomlLoader objects to properly parse values when inherited

--- src/lib/config.py
+++ src/lib/config.py
@@ -2,18 +2,14 @@ from lib.tomlFiles import TomlLoader, Any
 from pathlib import Path
 
 class Configs(TomlLoader):
-    def __init__(self, path: Path):
-        super().__init__(path)
+    def parse(self, value: any) -> Any:
+        if isinstance(value, str):
+            if value.startswith("./"):
+                return self._path.parent / value[2:]
 
-        for attrDict in self._data.values():
-            for key, value in attrDict._data.items():
-                if isinstance(value, str):
-                    if value.startswith("./"):
-                        value = path.parent / value[2:]
-
-                    elif value.startswith("/") or value[1:].startswith(":/"):
-                        value = Path(value)
-
-                attrDict._data[key] = value
+            if value.startswith("/") or value[1:].startswith(":/"):
+                return Path(value)
+        
+        return value
 
 globalConfig = Configs(Path(__file__).parents[2] / "config.toml")
