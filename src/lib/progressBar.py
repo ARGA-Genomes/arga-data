41874971681575bb707b0c04f5ebec943225a00c
Christopher Mangion <christopher.mangion@hotmail.com>
2025-06-30 13:43:49 +1000

    Updated ProgressBar to allow updating by any amount ProgressBar now properly prints a newline character at end of bar when bar is completed ProgressBar loading icon now rotates properly with variable calls per update

--- src/lib/progressBar.py
+++ src/lib/progressBar.py
@@ -19,16 +19,16 @@ class ProgressBar:
         return f"[{min(completedLength, percentagePos) * '='}{max(percentagePos - completedLength, 0) * '-'}{percentage}{max(completedLength - secondHalfStart, 0) * '='}{((self._length - secondHalfStart) - max(completedLength - secondHalfStart, 0)) * '-'}]"
 
     def _render(self, extraInfo: str) -> int:
-        output = f"> {self._processName}{' - ' if extraInfo else ''}{extraInfo} ({self._loading[self._atTask % len(self._loading)]}): {self._getBar()}"
-        print(output, end="\r")
+        output = f"> {self._processName}{' - ' if extraInfo else ''}{extraInfo} ({self._loading[(self._atTask // self._callsPerUpdate) % len(self._loading) ]}): {self._getBar()}"
+        print(output, end="\n" if self._atTask >= self._totalTasks else "\r")
         return len(output)
 
-    def update(self, extraInfo: str = "") -> int:
-        if self._atTask == self._totalTasks:
+    def update(self, updateAmount: int = 1, extraInfo: str = "") -> int:
+        if self._atTask >= self._totalTasks:
             return 0
         
-        self._atTask += 1
-        if self._atTask % self._callsPerUpdate == 0 or self._atTask == self._totalTasks:
+        self._atTask += updateAmount
+        if self._atTask % self._callsPerUpdate == 0 or self._atTask >= self._totalTasks:
             return self._render(extraInfo)
 
         return 0
