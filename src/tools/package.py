b69b5917febe6e7d987b49f8387225b656c4cd8b
Christopher Mangion <christopher.mangion@hotmail.com>
2025-07-15 13:56:43 +1000

    Updated package script to work again

--- src/tools/package.py
+++ src/tools/package.py
@@ -1,10 +1,8 @@
 from lib.data.argParser import ArgParser
-from lib.processing.files import Step
 
 if __name__ == '__main__':
     parser = ArgParser(description="Package converted data")
 
-    sources, args, prepArgs, execArgs = parser.parseArgs()
-    kwargs = parser.namespaceKwargs(args)
+    sources, flags, kwargs = parser.parseArgs()
     for source in sources:
         source.package()
