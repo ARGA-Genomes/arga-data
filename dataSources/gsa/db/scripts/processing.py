5ea1ba2995f01e07899c0e56a45f2729180dcf10
Christopher Mangion <christopher.mangion@hotmail.com>
2025-07-15 15:40:09 +1000

    GSA WIP

--- dataSources/gsa/db/scripts/processing.py
+++ dataSources/gsa/db/scripts/processing.py
@@ -1,24 +1,14 @@
 from lib.crawler import Crawler
 import pandas as pd
 from pathlib import Path
+import lib.downloading as dl
 
 def build(outputFilePath: Path) -> None:
     url = "http://download.cncb.ac.cn/gsa/"
     regexMatch = ".*\\.gz"
 
-    crawler = Crawler(outputFilePath.parent, url, regexMatch)
-    files, leftoverFolders = crawler.crawl()
-
-    if leftoverFolders:
-        print("Exited early, writing file/folder progress to file")
-
-        with open(outputFilePath.parent / "files.txt", 'w') as fp:
-            fp.write("\n".join(files))
-
-        with open(outputFilePath.parent / "folders.txt", 'w') as fp:
-            fp.write("\n".join(leftoverFolders))
-
-        exit()
+    crawler = Crawler(outputFilePath.parent)
+    crawler.run(url, regexMatch)
 
     data = []
     prefixes = {
@@ -29,20 +19,22 @@ def build(outputFilePath: Path) -> None:
         "CRR": "run_accession"
     }
 
-    for file in files:
-        path = file.strip(url)
-        info = {}
-        for item in path.split("/"):
-            if item.endswith(".gz"): # Reached file name, no new column
-                break
+    print(crawler.getFileURLs())
+    # for filesToDownload in crawler.getFileURLs():
+    #     dl.download()
+    #     path = file.strip(url)
+    #     info = {}
+    #     for item in path.split("/"):
+    #         if item.endswith(".gz"): # Reached file name, no new column
+    #             break
 
-            for prefix, column in prefixes.items():
-                if item.startswith(prefix):
-                    info[column] = item
-                    break
+    #         for prefix, column in prefixes.items():
+    #             if item.startswith(prefix):
+    #                 info[column] = item
+    #                 break
 
-        info |= {"url": file}
-        data.append(info)
+    #     info |= {"url": file}
+    #     data.append(info)
 
-    df = pd.DataFrame.from_records(data)
-    df.to_csv(outputFilePath, index=False)
+    # df = pd.DataFrame.from_records(data)
+    # df.to_csv(outputFilePath, index=False)
