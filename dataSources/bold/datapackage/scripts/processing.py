378f7304c9eca08ea0d08c9edb4c422ef9fb0a51
Christopher Mangion <christopher.mangion@hotmail.com>
2025-08-21 09:51:49 +1000

    Added a process for retrieving bold data package autonomously

--- dataSources/bold/datapackage/scripts/processing.py
+++ dataSources/bold/datapackage/scripts/processing.py
@@ -1,18 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
-
 import numpy as np
 import pandas as pd
 from pathlib import Path
+import logging
+from lib.secrets import secrets
+import lib.downloading as dl
 
 def retrieve(outputFilePath: Path):
-    # datapackageURL = "https://www.boldsystems.org/index.php/datapackages"
-    # request = requests.get(datapackageURL)
+    baseURL = "https://bench.boldsystems.org/index.php"
+    session = requests.Session()
 
-    # tables = pd.read_html(request.text)
-    # recentData = tables[0]
-    # mostRecent = recentData["Snapshot Date"][0]
-    mostRecent = "28-APR-2023"
+    logging.info("Collecting latest date for bold package")
+    response = session.get(f"{baseURL}/datapackages/Latest")
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    for header in soup.find_all("h2"):
+        text = header.text
+        if text.startswith("BOLD DNA Barcode Reference Library"):
+            break
+    else:
+        logging.error("No header found for retrieving date")
+        exit()
+
+    rawDate = text.rsplit(" ", 1)[-1]
+    day, month, year = rawDate.split("-")
+    date = f"{day}-{month.capitalize()}-{year}"
+    logging.info(f"Latest datapackage date: {date}")
+
+    logging.info("Retrieving uid for download")
+    datapackageURL = f"{baseURL}/API_Datapackage?id=BOLD_Public.{date}"
+    response = session.get(datapackageURL)
+    uid = response.text.strip('"')
+    downloadURL = f"{datapackageURL}&uid={uid}"
+
+    dl.download(downloadURL, outputFilePath, verbose=True, auth=dl.buildAuth(secrets.bold.username, secrets.bold.password))
 
 def cleanUp(folderPath: Path, outputFilePath: Path) -> None:
     for file in folderPath.iterdir():
