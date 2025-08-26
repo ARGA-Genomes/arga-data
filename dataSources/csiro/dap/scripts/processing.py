153d4c5a4f74a503e6b7ba77eed3e619106df49a
Christopher Mangion <christopher.mangion@hotmail.com>
2025-08-26 14:01:09 +1000

    Updated dap processing to function again Needs more work to retrieve proper data

--- dataSources/csiro/dap/scripts/processing.py
+++ dataSources/csiro/dap/scripts/processing.py
@@ -1,31 +1,25 @@
-import math
 import requests
 import pandas as pd
 from pathlib import Path
+import lib.dataframes as dff
 
 def getPortalData(outputFilePath: Path) -> None:
     baseURL = "https://data.csiro.au/dap/ws/v2/collections"
     entriesPerPage = 100
 
-    # Get page 1 first, then iterate over remaining pages
-    response = requests.get(f"{baseURL}?rpp={entriesPerPage}&p={1}")
-    data = response.json()
-    records = data["dataCollections"]
-
-    totalResults = data["totalResults"]
-    totalCalls = math.ceil(int(totalResults) / entriesPerPage)
-
-    for call in range(1, totalCalls):
-        response = requests.get(f"{baseURL}?rpp={entriesPerPage}&p={call+1}")
+    currentPage = 1
+    allRecords = []
+    while True:
+        print(f"Collecting page: {currentPage}", end="\r")
+        response = requests.get(f"{baseURL}?rpp={entriesPerPage}&p={currentPage}", headers={"Content-Type": "application/json"})
         data = response.json()
-        records.extend(data["dataCollections"])
+        records = data.get("dataCollections", [])
+        allRecords.extend(records)
+        currentPage += 1
 
-    for record in records:
-        record |= record.pop("spatialParameters", {})
+        if len(records) < entriesPerPage:
+            break
 
-    df = pd.DataFrame.from_records(records)
-    # idDf = df["id"].apply(lambda x: dict(x)).apply(pd.Series)
-    # df.drop("id", axis=1, inplace=True)
-    # df = pd.concat([idDf, df], axis=1)
-    df.replace(to_replace=[r"\\t|\\n|\\r", "\t|\n|\r"], value=["", ""], regex=True, inplace=True)
+    df = pd.DataFrame.from_records(allRecords)
+    df = dff.removeSpaces(df)
     df.to_csv(outputFilePath, index=False)
