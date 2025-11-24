32633bcd4069e5a1d07a10f5ea6a870b5be0e2c7
Christopher Mangion <christopher.mangion@hotmail.com>
2025-11-24 15:08:41 +1000

    Added JsonSynchronizer to sync metadata easily Updated DFWriter and RecordWriter to use JsonSynchronizer to store metadata for preserving progress between runs Removed getMatchingPaths method from DataFile as it is no longer necessary

--- src/lib/processing/files.py
+++ src/lib/processing/files.py
@@ -202,9 +202,6 @@ class Folder(FileObject):
         cmn.clearFolder(self._backupPath, True)
         self._backupPath = None
 
-    def getMatchingPaths(self, pattern: str) -> Iterator[Path]:
-        return self.path.glob(pattern)
-
 class StackedFile(Folder, DataFile):
 
     format = DataFormat.STACKED
