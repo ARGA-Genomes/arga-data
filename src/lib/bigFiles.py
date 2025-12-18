bf8267fb7a3fbf46114e68ecb682dd3e32953d62
Christopher Mangion <christopher.mangion@hotmail.com>
2025-12-18 15:49:57 +1000

    Chunk memory management WIP

--- src/lib/bigFiles.py
+++ src/lib/bigFiles.py
@@ -129,7 +129,8 @@ def combineDirectoryFiles(outputFilePath: Path, inputFolderPath: Path, matchPatt
 def combineDataFiles(outputFilePath: Path, dataFiles: list[DataFile], columns: list[str], chunkSize: int = 1024, deleteOld: bool = False, **kwargs: dict) -> None:
     outputDataFile = DataFile(outputFilePath)
     logging.info(f"Combining into one file at {outputFilePath}")
-    outputDataFile.writeIterator(combinedIterator(dataFiles, chunkSize), columns, index=False, **kwargs)
+    iterator = combinedIterator(dataFiles, chunkSize)
+    outputDataFile.writeIterator(iterator, columns, index=False, **kwargs)
     logging.info(f"Successfully combined into a single file")
 
     if deleteOld:
