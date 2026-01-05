7a464866d1a97f15d313eb174f77fd9ebc368e04
Christopher Mangion <christopher.mangion@hotmail.com>
2026-01-05 11:11:47 +1000

    Added tasks library for handling processing tasks

---
+++ src/lib/processing/tasks.py
@@ -0,0 +1,138 @@
+from pathlib import Path
+import logging
+from lib.processing.files import DataFile
+from lib.processing.scripts import OutputScript
+import lib.downloading as dl
+from lib.crawler import Crawler
+
+class Task:
+    def __init__(self):
+        self._runMetadata = {}
+        self._outputs: list[DataFile] = []
+
+    def getOutputs(self) -> list[DataFile]:
+        return self._outputs
+
+    def run(self, overwrite: bool, verbose: bool) -> bool:
+        return True
+    
+    def setAdditionalMetadata(self, metadata: dict) -> None:
+        self._runMetadata.update(metadata)
+
+    def getMetadata(self) -> dict:
+        return self._runMetadata
+
+class UrlRetrieve(Task):
+
+    _url = "url"
+    _name = "name"
+    _properties = "properties"
+
+    def __init__(self, workingDir: Path, username: str, password: str, config: dict):
+        self.workingDir = workingDir
+        self.username = username
+        self.password = password
+
+        self.url = config.get(self.url, None)
+        if self.url is None:
+            raise Exception("No url provided for source") from AttributeError
+
+        fileName = config.get(self._name, None)
+        if fileName is None:
+            raise Exception("No filename provided to download to") from AttributeError
+    
+        properties = config.get(self._properties, {})
+        self.file = DataFile(self.workingDir / fileName, properties)
+
+        super().__init__([self.file])
+
+    def getOutputs(self) -> list[DataFile]:
+        return [self.file]
+
+    def run(self, overwrite: bool, verbose: bool) -> bool:
+        if not overwrite and self.file.exists():
+            logging.info(f"Output file {self.file.path} already exists")
+            return False
+        
+        self.file.delete()
+        return dl.download(self.url, self.file.path, verbose=verbose, auth=dl.buildAuth(self.username, self.password))
+
+class CrawlRetrieve(Task):
+
+    _url = "url"
+    _regex = "regex"
+    _link = "link"
+    _maxDepth = "maxDepth"
+    _properties = "properties"
+    _filenameURLParts = "urlPrefix"
+
+    def __init__(self, workingDir: Path, username: str, password: str, config: dict, overwrite: bool):
+        self.workingDir = workingDir
+        self.username = username
+        self.password = password
+
+        url = config.pop(self._url, None)
+        regex = config.pop(self._regex, None)
+        link = config.pop(self._link, "")
+        maxDepth = config.pop(self._maxDepth, -1)
+        filenameURLParts = config.pop(self._filenameURLParts, 1)
+        properties = config.pop(self._properties, {})
+
+        crawler = Crawler(self.workingDir, dl.buildAuth(self.username, self.password))
+        crawler.run(url, regex, maxDepth, overwrite)
+        urlList = crawler.getFileURLs(link)
+
+        self.downloads: list[tuple[str, DataFile]] = []
+        for url in urlList:
+            fileName = "_".join(url.split("/")[-filenameURLParts:])
+            self.downloads.append((url, DataFile(self.workingDir / fileName, properties)))
+
+        super().__init__([downloadFile for _, downloadFile in self.downloads])
+
+    def run(self, overwrite: bool, verbose: bool) -> bool:
+        downloadsRun = False
+        for downloadURL, downloadFile in self.downloads:
+            if not overwrite and downloadFile.exists():
+                continue
+
+            downloadFile.delete()
+            dl.download(downloadURL, downloadFile.path, auth=dl.buildAuth(self.username, self.password), verbose=verbose)
+            downloadsRun = True
+
+        return downloadsRun
+
+class ScriptRunner(Task):
+
+    _modulePath = "path"
+    _functionName = "function"
+    _inputs = "inputs"
+    _args = "args"
+    _kwargs = "kwargs"
+    _outputs = "outputs"
+
+    def __init__(self, workingDir: Path, config: dict, libraryDirs: dict):
+        modulePath = config.pop(self._modulePath, "")
+        if not modulePath:
+            raise Exception("No `path` specified in script config") from AttributeError
+
+        functionName = config.pop(self._functionName, "")
+        if not functionName:
+            raise Exception("No `function` specified in script config") from AttributeError
+
+        outputs = config.pop(self._outputs, [])
+        if not outputs:
+            raise Exception("No `outputs` specified in script config") from AttributeError        
+
+        outputs = [DataFile(workingDir / output) for output in outputs]
+        inputs = [DataFile(input) for input in config.pop(self._inputs, [])]
+
+        self.args = config.pop(self._args, [])
+        self.kwargs = config.pop(self._kwargs, {})
+
+        self.script = OutputScript(modulePath, functionName, outputs, inputs, libraryDirs)
+
+        super().__init__(outputs)
+
+    def run(self, overwrite: bool, verbose: bool) -> bool:
+        success, _ = self.script.run(overwrite, verbose, self.args, self.kwargs)
+        return success
