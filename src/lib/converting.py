d1ae5ab0476d711450d14bd83d66821913bab1f9
Christopher Mangion <christopher.mangion@hotmaill.com>
2026-05-06 15:03:08 +1000

    Simplified converter to no longer worry about nested RDF statements Added apply method that will translate the input dataframe

--- src/lib/converting.py
+++ src/lib/converting.py
@@ -4,33 +4,25 @@ import logging
 from lib.bigFiles import StackedDFWriter
 import gc
 from pathlib import Path
-from pyoxigraph import Store, RdfFormat, NamedNode, Triple
+from pyoxigraph import Store, RdfFormat, NamedNode
+import hashlib
 
 class Converter:
-
-    _mapFileName = "map.json"
-
-    _datasetIDLabel = "dataset_id"
-    _entityIDLabel = "entity_id"
-    _entityIDEvent = "collection"
-
     def __init__(self, inputFile: DataFile, outputPath: Path, mapPath: Path):
         self.inputFile = inputFile
         self.outputPath = outputPath
         self.mapPath = mapPath
-        
-        self._map = {}
+
+        self._map: dict[str, dict[str, list[tuple[str, str]]]] = {}
+
+    @staticmethod
+    def _hash(value: any) -> str:
+        return hashlib.md5(str(value).encode("utf-8")).hexdigest()
 
     def _loadMap(self) -> None:
         def _nodeName(node: NamedNode):
             return node.value.rsplit("/", 1)[-1]
-
-        def _addData(graphName: str, name: str, data: dict) -> None:
-            if name not in self.map[graphName]:
-                self.map[graphName][name] = {}
-
-            self.map[graphName][name] |= data
-
+        
         store = Store()
         with open(self.mapPath, "rb") as fp:
             store.load(fp, RdfFormat.TRIG)
@@ -40,26 +32,28 @@ class Converter:
             if graphName not in self.map:
                 self.map[graphName] = {}
 
-            for quad in store.quads_for_pattern(None, None, None, graph):
-                if isinstance(quad.object, NamedNode): # Normal translation
-                    nodeName = _nodeName(quad.subject)
-                    _addData(graphName, nodeName, {"method": _nodeName(quad.predicate), "source": _nodeName(quad.object)})
+            for quad in reversed(store.quads_for_pattern(None, None, None, graph)):
+                newColumn = _nodeName(quad.subject)
+                if newColumn not in self.map[graphName]:
+                    self.map[graphName][newColumn] = []
 
-                elif isinstance(quad.object, Triple): # Other requirement
-                    for nestedQuad in store.quads_for_pattern(None, None, quad.subject, graph):
-                        nodeName = _nodeName(nestedQuad.subject)
-                        _addData(graphName, nodeName, {"condition_method": _nodeName(nestedQuad.predicate), "condition": [_nodeName(quad.object.subject), _nodeName(quad.object.predicate), _nodeName(quad.object.object)]})
-    
+                self.map[graphName][newColumn].append((_nodeName(quad.predicate), _nodeName(quad.object)))
+
     def _apply(self, df: pd.DataFrame) -> dict[str, pd.DataFrame]:
-        ...
+        mappedData = {}
+        for section, columnInfo in self._map.items():
+
+            sectionData = {}
+            for newColumn, columnnSources in columnInfo.items():
+                for method, source in columnnSources:
+                    sectionData[newColumn] = df[source] if method == "same" else df[source].apply(self._hash)
+
+            mappedData[section] = pd.DataFrame.from_dict(sectionData)
+        return mappedData
 
     def convert(self, chunkSize: int, verbose: bool) -> tuple[bool, dict]:
-        self._loadMap()
-
-        logging.info("Processing chunks for conversion")
-        
-
-        writer = StackedDFWriter(self.outputPath, list(self.map))
+        map = self._loadMap()
+        writer = StackedDFWriter(self.outputPath, list(map))
 
         totalRows = 0
         chunks = self.inputFile.readIterator(chunkSize, low_memory=False)
@@ -68,6 +62,7 @@ class Converter:
         if completed > 0:
             logging.info(f"Already completed {completed} chunks, resuming...")
 
+        logging.info("Processing chunks for conversion")
         for idx, df in enumerate(chunks, start=1):
             totalRows += len(df)
 
