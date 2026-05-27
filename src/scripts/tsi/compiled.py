f2729223c3ec7facff353a893b51a71b92b84b43
Christopher Mangion <christopher.mangion@hotmaill.com>
2026-05-27 13:44:31 +1000

    Updated tsi compiled to properly compile a single file from source google sheet Added tsi mapping file Updated tsi compiled config to point to correct mapping file and input file

--- src/scripts/tsi/compiled.py
+++ src/scripts/tsi/compiled.py
@@ -1,19 +1,33 @@
 from pathlib import Path
 import lib.downloading as dl
 from lib.processing.scripts import importableScript
+import pandas as pd
 
 @importableScript(inputCount=0)
 def collectFromSheets(outputDir: Path):
     documentID = "1xy15ARqq8_0WRmTY7xcMz3giBwfs0iFOU6dMWjQN-oo"
-    
     sheetTabs = {
+        "livestate": 1334260048,
         "organisms": 0,
         "collecting": 1174387857,
         "tissues": 1559676056,
-        "lab_samples": 2130570893
+        "lab_samples": 2130570893,
+        "data_products": 1700782579
     }
 
-    outputDir.mkdir(exist_ok=True)
-    for fileName, sheetID in sheetTabs.items():
-        df = dl.getGoogleSheet(documentID, sheetID)
-        df.to_csv(outputDir / f"{fileName}.csv", index=False)
+    df = dl.getGoogleSheet(documentID, sheetTabs["organisms"])
+
+    def mergeSheet(df: pd.DataFrame, sheetName: str, mergeLeft: str, mergeRight: str = "") -> pd.DataFrame:
+        df2 = dl.getGoogleSheet(documentID, sheetTabs[sheetName])
+        colDiff = df2.columns.difference(df.columns).to_list()
+        if not mergeRight:
+            return df.merge(df2[[mergeLeft] + colDiff], "left", mergeLeft)
+        return df.merge(df2[colDiff], "left", left_on=mergeLeft, right_on=mergeRight)
+
+    df = mergeSheet(df, "livestate", "organism:organism_id")
+    df = mergeSheet(df, "collecting", "organism:organism_id", "collecting:organism_id")
+    df = mergeSheet(df, "tissues", "organism:organism_id", "tissues:organism_id")
+    df = mergeSheet(df, "lab_samples", "tissues:tissue_id")
+    df = mergeSheet(df, "data_products", "lab_sample:extract_id")
+
+    df.to_csv(outputDir / "tsiCompiled.csv", index=False)
